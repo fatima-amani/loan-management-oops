69e18a5f5c01fe8605a22ad354e1a977df7476cc
fatima <fatima.amani65@gmail.com>
2025-08-26 17:44:24 +0530

    added classes for personal and home loan

---
+++ loans/HomeLoan.java
@@ -0,0 +1,25 @@
+package loans;
+
+import loans.enums.LoanStatus;
+import loans.enums.LoanType;
+
+public class HomeLoan extends LoanApplication implements Approvable{
+    public HomeLoan(String customerName, Float amount, LoanType loanType, LoanStatus loanStatus) {
+        super(customerName, amount, loanType, loanStatus);
+    }
+
+    @Override
+    public LoanStatus evaluateRisk() {
+        if(amount < 5000000) {
+            loanStatus=LoanStatus.APPROVED;
+        } else {
+            loanStatus=LoanStatus.REJECTED;
+        }
+        return loanStatus;
+    }
+
+    @Override
+    boolean validateApplication() {
+        return loanStatus==LoanStatus.APPROVED;
+    }
+}
---
+++ loans/PersonalLoan.java
@@ -0,0 +1,26 @@
+package loans;
+
+import loans.enums.LoanStatus;
+import loans.enums.LoanType;
+
+public class PersonalLoan extends LoanApplication implements Approvable{
+
+    public PersonalLoan(String customerName, Float amount, LoanType loanType, LoanStatus loanStatus) {
+        super(customerName, amount, loanType, loanStatus);
+    }
+
+    @Override
+    public LoanStatus evaluateRisk() {
+        if(amount > 500000) {
+            loanStatus=LoanStatus.REJECTED;
+        } else {
+            loanStatus=LoanStatus.APPROVED;
+        }
+        return loanStatus;
+    }
+
+    @Override
+    boolean validateApplication() {
+        return loanStatus == LoanStatus.APPROVED;
+    }
+}
